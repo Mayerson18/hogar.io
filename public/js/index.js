27ddec6b75dcfa6e172868063d559176878ae298
Mayerson <mayerson18@gmail.com>
2017-08-14 22:55:45 -0400

    previa

---
+++ public/js/index.js
@@ -0,0 +1,19 @@
+"use strict";
+$(".nav-toggle").click(function() {
+  if($(".nav-menu").hasClass("is-active")){
+    $(".nav-menu").removeClass("is-active");
+    $(".nav-menu").children().removeClass("r");
+  }else{
+    $(".nav-menu").addClass("is-active");
+    $(".nav-menu").children().addClass("r");
+  }
+});
+
+var someBeforeOrAfterFunction = function(e, animationContext){
+
+	//The event that triggers the animation
+	console.log(e);
+
+	//The Animation Context Instance
+	console.log(animationContext);
+}
