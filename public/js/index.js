772c11a6dae0e7bccbda3a2b208ea5333592ef6a
Mayerson <mayerson18@gmail.com>
2017-08-18 10:32:32 -0400

    duraciion

--- public/js/index.js
+++ public/js/index.js
@@ -18,7 +18,13 @@ var someBeforeOrAfterFunction = function(e, animationContext){
 	console.log(animationContext);
 }
 
+
+
 $(".click").click(function(){
+  $(".modal").addClass("fadeIn animated");
+})
+
+$(".click1").click(function(){
   $(".modal1").addClass("is-active");
 })
 
@@ -28,10 +34,12 @@ $(".click2").click(function(){
 
 $(".modal-background").click(function(){
   $(".modal").removeClass("is-active");
+  $(".modal").removeClass("fadeIn animated");
 })
 
 $(".modal-close").click(function(){
   $(".modal").removeClass("is-active");
+  $(".modal").removeClass("fadeIn animated");
 })
 
 !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0";
